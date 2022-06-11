7ec34307cbe5fae7ad2729a62ea6cc42bcb6b377
v131v <jsjsjs87@mail.ru>
2022-06-12 02:59:35 +0500

    feat(configs): add base config

---
+++ configs/base.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+};
